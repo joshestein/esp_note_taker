e2b051147ab261f7d54c095fbac1862fb1c7e5a3
Josh Stein <josh.e.stein@gmail.com>
2026-07-02 19:28:17 +0200

    Initial state machine with app-level state

--- main/main.c
+++ main/main.c
@@ -1,8 +1,40 @@
-#include <stdio.h>
+#include "button.h"
 #include "driver/gpio.h"
+#include <stdio.h>
 
+typedef enum {
+  IDLE = 0,
+  RECORDING,
+  POST_SAVE,
+} app_state_t;
 
-void app_main(void)
-{
-    // TODO
+void app_main(void) {
+  app_state_t state = IDLE;
+  EventGroupHandle_t button_group = button_init();
+
+  for (;;) {
+    EventBits_t uxBits = xEventGroupWaitBits(
+        button_group, BOOT_BUTTON_BIT | POWER_BUTTON_BIT,
+        pdTRUE,  /* Clear before returning. */
+        pdFALSE, /* Don't wait for both bits, either bit will do. */
+        portMAX_DELAY);
+
+    if ((uxBits & BOOT_BUTTON_BIT) != 0) {
+        printf("Boot button pressed\n");
+        if (state == IDLE) {
+            state = RECORDING;
+        } else if (state == RECORDING) {
+            state = POST_SAVE;
+        }
+    } else if ((uxBits & POWER_BUTTON_BIT) != 0) {
+      printf("Power button pressed\n");
+    }
+
+    if (state == POST_SAVE) {
+        printf("Saving data...\n");
+        vTaskDelay(pdMS_TO_TICKS(2000));
+        printf("Data saved. Returning to IDLE state.\n");
+        state = IDLE;
+    }
+  }
 }
