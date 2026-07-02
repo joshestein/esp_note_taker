86d7ae4723200d5b5c2718abd3ebc4bc69f37c53
Josh Stein <josh.e.stein@gmail.com>
2026-07-02 19:10:13 +0200

    Initial interrupt handlers for both buttons

--- components/button/button.c
+++ components/button/button.c
@@ -1,7 +1,60 @@
-#include <stdio.h>
 #include "button.h"
 #include "config.h"
+#include "driver/gpio.h"
+#include <stdio.h>
 
-void button_init(void) {
+static EventGroupHandle_t button_groups;
 
+static void IRAM_ATTR boot_button_handler(void* arg) {
+  BaseType_t xHigherPriorityTaskWoken, xResult;
+  xHigherPriorityTaskWoken = pdFALSE;
+  xResult = xEventGroupSetBitsFromISR(button_groups, BOOT_BUTTON_BIT,
+                                      &xHigherPriorityTaskWoken);
+  if (xResult == pdPASS) {
+    // If xHigherPriorityTaskWoken is now set to pdTRUE then a context
+    // switch should be requested.  The macro used is port specific and
+    // will be either portYIELD_FROM_ISR() or portEND_SWITCHING_ISR() -
+    // refer to the documentation page for the port being used.
+    portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+  }
+}
+
+static void IRAM_ATTR power_button_handler(void* arg) {
+  BaseType_t xHigherPriorityTaskWoken, xResult;
+  xHigherPriorityTaskWoken = pdFALSE;
+  xResult = xEventGroupSetBitsFromISR(button_groups, POWER_BUTTON_BIT,
+                                      &xHigherPriorityTaskWoken);
+  if (xResult == pdPASS) {
+    // If xHigherPriorityTaskWoken is now set to pdTRUE then a context
+    // switch should be requested.  The macro used is port specific and
+    // will be either portYIELD_FROM_ISR() or portEND_SWITCHING_ISR() -
+    // refer to the documentation page for the port being used.
+    portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+  }
+}
+
+static void gpio_init(void) {
+  gpio_config_t gpio_conf = {};
+  gpio_conf.intr_type = GPIO_INTR_NEGEDGE;
+  gpio_conf.mode = GPIO_MODE_INPUT;
+  gpio_conf.pin_bit_mask = (1ULL << BOOT_BUTTON) | (1ULL << POWER_BUTTON);
+  gpio_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
+  gpio_conf.pull_up_en = GPIO_PULLUP_ENABLE;
+
+  ESP_ERROR_CHECK_WITHOUT_ABORT(gpio_config(&gpio_conf));
+}
+
+EventGroupHandle_t button_init(void) {
+  button_groups = xEventGroupCreate();
+
+  gpio_init();
+
+  gpio_wakeup_enable(BOOT_BUTTON, GPIO_INTR_LOW_LEVEL);
+  gpio_wakeup_enable(POWER_BUTTON, GPIO_INTR_LOW_LEVEL);
+
+  gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
+  gpio_isr_handler_add(BOOT_BUTTON, boot_button_handler, (void *)BOOT_BUTTON);
+  gpio_isr_handler_add(POWER_BUTTON, power_button_handler, (void *)POWER_BUTTON);
+
+  return button_groups;
 }
