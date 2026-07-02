358a6255839bcfb1d3656b7547264b45e033543d
Josh Stein <josh.e.stein@gmail.com>
2026-07-02 19:10:01 +0200

    Define bit masks for each button

--- components/button/button.h
+++ components/button/button.h
@@ -4,6 +4,9 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/event_groups.h"
 
+#define BOOT_BUTTON_BIT (1 << 0)
+#define POWER_BUTTON_BIT (1 << 1)
+
 EventGroupHandle_t button_init(void);
 
 #endif
