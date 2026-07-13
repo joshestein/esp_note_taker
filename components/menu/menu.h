d457eb0fbedd3aca031bbbfd50c9d12af3b25b1d
Josh Stein <josh.e.stein@gmail.com>
2026-07-13 19:25:32 +0200

    `menu_init` returns err

--- components/menu/menu.h
+++ components/menu/menu.h
@@ -1,6 +1,7 @@
 #ifndef MENU_H
 #define MENU_H
 
+#include "esp_err.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/event_groups.h"
 #include <stdint.h>
@@ -11,7 +12,7 @@ typedef enum {
   MENU_INTENT_SLEEP,
 } menu_intent_t;
 
-void menu_init(EventGroupHandle_t button_group);
+esp_err_t menu_init(EventGroupHandle_t button_group);
 void menu_enter(void);
 menu_intent_t menu_act(void);
 void menu_step(void);
